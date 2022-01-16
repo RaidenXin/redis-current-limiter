a098eb57af233c937a5078357a1798a2313dfa59
Raiden <549485630@qq.com>
2022-01-16 18:12:51 +0800

    加入注释

--- src/main/java/com/raiden/redis/current/limiter/config/RedisCurrentLimiterConfiguration.java
+++ src/main/java/com/raiden/redis/current/limiter/config/RedisCurrentLimiterConfiguration.java
@@ -4,6 +4,8 @@ import com.raiden.redis.current.limiter.RedisCurrentLimiterInit;
 import com.raiden.redis.current.limiter.aop.RedisCurrentLimitingAspect;
 import com.raiden.redis.current.limiter.properties.RedisCurrentLimiterProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
@@ -13,6 +15,10 @@ import org.springframework.context.annotation.Bean;
  * @Date:Created in 23:51 2020/8/27
  * @Modified By:
  */
+@EnableConfigurationProperties(RedisCurrentLimiterProperties.class)
+@ConditionalOnProperty(
+        name = {"redis.current-limiter.enabled"}
+)
 public class RedisCurrentLimiterConfiguration {
 
     @Bean
@@ -21,7 +27,6 @@ public class RedisCurrentLimiterConfiguration {
     }
 
     @Bean
-    @ConditionalOnBean(RedisCurrentLimiterProperties.class)
     public RedisCurrentLimitingAspect redisCurrentLimitingAspect(RedisCurrentLimiterProperties properties, ApplicationContext context){
         return new RedisCurrentLimitingAspect(context, properties);
     }
