c2161747abaeba742939b93c829fb468e8c7280d
Raiden <549485630@qq.com>
2022-01-16 19:23:03 +0800

    加入注释

--- src/main/java/com/raiden/redis/current/limiter/RedisCurrentLimiter.java
+++ src/main/java/com/raiden/redis/current/limiter/RedisCurrentLimiter.java
@@ -46,21 +46,32 @@ public final class RedisCurrentLimiter {
         if (path == null || path.isEmpty()){
             throw new IllegalArgumentException("The path parameter cannot be empty !");
         }
-        final String key = new StringBuffer(CURRENT_LIMITER).append(path).toString();
+        final byte[] keyByte = new StringBuffer(CURRENT_LIMITER).append(path).toString().getBytes(CHARSET);
         long now = System.currentTimeMillis();
         long windowTimeMillis = windowTimeUnit.toMillis(windowTime);
         //先获取当前窗口请求标记总数 如果超过最大值直接 返回不予许访问
-        ZSetOperations zSetOperations = redis.opsForZSet();
-        Long count = zSetOperations.zCard(key);
-        if (count > maxCount){
+        List<List<Object>> resultArr = redis.executePipelined((RedisConnection connection) -> {
+            //开启事物保障这些命令同一批次串行执行
+            connection.multi();
             //删除过期窗口中的 请求标记
-            zSetOperations.removeRangeByScore(key, 0, now - windowTimeMillis);
+            connection.zRemRangeByScore(keyByte, 0, now - windowTimeMillis);
+            //获取当前窗口请求标记总数
+            connection.zCard(keyByte);
+            //设置窗口过期时间
+            connection.pExpire(keyByte, windowTimeMillis + 1000);
+            //执行事物
+            connection.exec();
+            return null;
+        });
+        long count = getCount(resultArr, 0, 3, 1);
+        if (count >= maxCount){
+            System.out.println("这里是:" + count);
+            //删除过期窗口中的 请求标记
             return false;
         }else {
             //如果窗口中的访问数 比最大访问数小
-            byte[] keyByte = key.getBytes(CHARSET);
             //通过通道执行命令
-            List<List<Object>> arrList = redis.executePipelined((RedisConnection connection) -> {
+            resultArr = redis.executePipelined((RedisConnection connection) -> {
                 //开启事物保障这些命令同一批次串行执行
                 connection.multi();
                 //插入一个请求标记
@@ -75,23 +86,28 @@ public final class RedisCurrentLimiter {
                 connection.exec();
                 return null;
             });
-            if (arrList.isEmpty()){
-                throw new RuntimeException("Error return result !");
-            }
-            List<Object> result = arrList.get(0);
-            if (result.size() != 4){
-                throw new RuntimeException("Error return result !");
-            }
-            Object o = result.get(2);
-            if (o instanceof Long){
-                return ((Long) o).longValue() <= maxCount;
-            }else {
-                throw new RuntimeException("Error return result !");
-            }
         }
+        return getCount(resultArr, 0, 4, 2) <= maxCount;
     }
 
     private static byte[] createValue(long now){
         return new StringBuilder(ip).append(now).append(Math.random() * 100).toString().getBytes(CHARSET);
+    }
+
+    private static long getCount(List<List<Object>> resultArr, int index,int size,int countIndex){
+        if (resultArr.isEmpty()){
+            throw new RuntimeException("Error return result !");
+        }
+        List<Object> result = resultArr.get(index);
+        if (result.size() != size){
+            throw new RuntimeException("Error return result !");
+        }
+        Object o = result.get(countIndex);
+        if (o instanceof Long){
+            System.out.println("返回值是：" + o);
+            return ((Long) o).longValue();
+        }else {
+            throw new RuntimeException("Error return result !");
+        }
     }
 }
